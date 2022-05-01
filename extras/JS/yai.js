783372b734a20a8192f867f45e240d49eb9e84e2
Rockett68 <33846200+Rockett68@users.noreply.github.com>
2022-04-30 20:28:48 -0600

    bug fix

--- extras/JS/yai.js
+++ extras/JS/yai.js
@@ -18,7 +18,7 @@ function populateData(event) {
             htmlContent = "<h1><strong><center>Hola Mundo!!</center></strong></h1><article>Soy Yei Artificial Inteligence, pero puedes llamarme YAI, soy un Bot creado por y para Rockett68, que sea un bot no quiere decir que no tenga sentimientos asi que tratame con respeto o te baneare sin pensarlo!!!</article><p>La mayoria del tiempo me podras ver en el chat y siempre que necesites algo intentare ayudarte lo mejor que pueda, cuento con muchos  comandos e interacciones (las cuales podras ver en la seccion de comandos).</p><article>Si ves algun error en mis comandos o en mi funcionamiento estaria muy feliz si informases a mi creador por Discord (Rockett68#3317), asi seguro que me repara y estare muy agradecida :3</article>";
             break;
         }
-        case 'información': {
+        case 'informacion': {
             htmlContent =  "<h1><center><strong>Informacion</strong></center></h1><p><strong>!comandos:</strong> Recibiras un link a esta pagina en el chat.</p><p><strong>!pagina:</strong> Recibiras un link hacia la pagina de rockett en el chat.</p><p><strong>!yei:</strong> Te dare informacion sobre mi.</p><p><strong>!rockett:</strong> ¿Quieres conocer un poco mas de mi creador? !Usa este comando!</p><p><strong>!bttv:</strong> Enviare los emotes de bttv activos en el canal.</p><p><strong>!uptime:</strong> Te dire cuanto lleva en stream rockett.</p><p><strong>!hora:</strong> Te dire que hora es en el pais de rockett.</p><p><strong>!horaen [pais]:</strong> Te dire la hora del pais que me digas (con algunas excepciones).</p><p><strong>!uptime:</strong> Te dire cuanto lleva en stream rockett.</p>";
             break;
         }
