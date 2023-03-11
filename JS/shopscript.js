82cec76c41bd22be557fc4de016706fd882a054a
Rockett68 <33846200+Rockett68@users.noreply.github.com>
2023-03-11 08:44:44 -0800

    .

---
+++ JS/shopscript.js
@@ -0,0 +1,26 @@
+var modal = document.getElementById("myModalContact");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+
+function openModal(imageSrc) {
+  modal.style.display = "block";
+  img.src = imageSrc;
+  captionText.innerHTML = img.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+function closeModal() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
